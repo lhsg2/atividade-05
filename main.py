cb26b40499b7e88a26240b7d1c0173b7667735ed
Luiz Henrique <138501027+lhsg2@users.noreply.github.com>
2024-06-22 11:49:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+n1 = float(input("degite um número:"))
+n2 = float(input("degite outro número:"))
+if n1> n2:
+  print(f"{n1} é maior que {n2}")
+if n1 == n2:
+  print(f"{n1} é igual a {n2}")
+else:
+  print(f"{n2} é maior que {n1}")
